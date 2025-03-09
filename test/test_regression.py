d5617006685d08c7426e725e8876c7c7ee159a7e
Tamika Nomara <taminomara@gmail.com>
2025-03-09 15:44:43 +0400

    >_<

--- test/test_regression.py
+++ test/test_regression.py
@@ -4,7 +4,7 @@ import pytest
 from bs4 import BeautifulSoup
 
 
-@pytest.mark.sphinx("html", testroot="doc")
+@pytest.mark.sphinx("html", testroot="doc", verbosity=2)
 @pytest.mark.parametrize(
     "src",
     [
