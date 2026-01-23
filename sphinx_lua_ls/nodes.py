fa7ddc7ee26117e2a7741afb0b3cb1cba85a6990
Tamika Nomara <taminomara@gmail.com>
2026-01-23 16:50:16 +0400

    Fix code style

--- sphinx_lua_ls/nodes.py
+++ sphinx_lua_ls/nodes.py
@@ -1,21 +1,23 @@
-from sphinx import addnodes
-from docutils import nodes
 from typing import Any
+
+from docutils import nodes
+from sphinx import addnodes
 
 
 class SigIndentNode(addnodes.desc_sig_space):
     def __init__(
         self,
-        rawsource: str = '',
-        text: str = '    ',
+        rawsource: str = "",
+        text: str = "    ",
         *children: nodes.Element,
-        **attributes: Any
+        **attributes: Any,
     ):
         super().__init__(rawsource, text, *children, **attributes)
 
 
 def visit_sig_indent_latex(self, node):
     self.body.append("~~~~")
+
 
 def depart_sig_indent_latex(self, node):
     pass
